b1eeaf17fd56404e51e42ae9fa315262cb33c2b5
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-13 17:52:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+    - The following function should return the number of times searchString appears in the variable listOfStrings
+    - listOfStrings is should be an array of strings
+*/
+function countWords(listOfStrings, searchString) {
+    let count = 0;
+
+    return count
+}
+
+const searchString = "love";
+const arrayOfStrings = ["I love to eat pizza","I love love to code"]
+
+console.log(countWords(arrayOfStrings, "love"));
+
