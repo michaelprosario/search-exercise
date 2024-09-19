3e429fdd6ab9e986f071121a2281956ca2926425
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-19 14:23:52 +0000

    Refactor countWords function and add test cases

--- index.js
+++ index.js
@@ -5,11 +5,19 @@
 function countWords(listOfStrings, searchString) {
     let count = 0;
 
+    // feel free to add console.log statements to debug your code
+
     return count
 }
 
 const searchString = "love";
 const arrayOfStrings = ["I love to eat pizza","I love love to code"]
 
-console.log(countWords(arrayOfStrings, "love"));
+const count = countWords(arrayOfStrings, searchString);
+console.log("count = " + count);
+if(count === 3) {
+    console.log("Test passed");
+} else {
+    console.log("Test failed");
+}
 
